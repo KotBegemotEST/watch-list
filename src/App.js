4c23c00c3d9b8a6c172b4ed7e00a778b7061ee05
AntonBuketovEST <buketov.95@gmail.com>
2022-06-07 15:50:07 +0300

    themeSwitcher ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from "react";
+// import {Helmet,HelmetProvider} from "react-helmet-async"
+import DarkMode from "./components/DarkMode"
+
+// const useLocalState = (key, defaultValue) =>{
+//   const [value,setValue] = useState(
+    
+//     ()=>{
+//       const storedValue = localStorage.getItem(key)
+//       return storedValue === null ? defaultValue : JSON.parse(storedValue);
+//     });
+
+//     useEffect(()=>{
+//       const listener = (e) =>{
+//         console.log(e)
+//         if(e.storageArea == localStorage && e.key == key){
+//           setValue(JSON.parse(e.newValue))
+
+//         }
+//       }
+//         window.addEventListener("storage", listener)
+
+//       return () => {
+//         window.removeEventListener("storage",listener)
+//       }
+
+//     },[key]) 
+
+//   const setValueInLocalStorage = (newValue)=>{
+//     setValue((curentValue)=>{
+
+//         const result = typeof newValue === "function" ? newValue(curentValue):newValue
+//       localStorage.setItem(key,JSON.stringify(result))
+//         console.log(result)
+//         return result
+//     })
+//   }
+
+//    return [ value, setValueInLocalStorage ]
+
+// }
+
+
 
 function App() {
+  // const [username,setUsername] = useLocalState("username","")
+  // const [theme,setTheme] = useLocalState("theme","light")
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DarkMode/>
+    {/* <input value={username} onChange={(e)=>setUsername(e.target.value)} /> */}
     </div>
   );
 }
